01a473d51db4113d147b11f1c83486a7fadf2c15
Ara404E <119130650+Ara404E@users.noreply.github.com>
2024-02-22 21:14:48 +0300

    added a css file also all 3 buttons are working

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,87 +1,75 @@
 
+    const rock=document.querySelector('#rock');
+    const paper=document.querySelector('#paper');
+    const scissors=document.querySelector('#scissors');
+    const weaponDiv=document.querySelector('.weaponDiv');
+    const outCome=document.querySelector('.outcome');
+    const result=document.querySelector('.result');
+    const computer=document.querySelector('.computerScore');
+    const player=document.querySelector(".PlayerScore");
+    
+let playerScore=0;
+let computerScore=0;
+
+
+
 function getComputerChoice(){
         let choices=['Rock','Paper','Scissors']
         let picked=choices[Math.floor(Math.random() * choices.length)];
         return picked.toLowerCase()
 }
-let playerWon=0;
-let computerWon=0;
 
-function playRound(playerSelection,ComputerSelection){
-    playerSelection=playerSelection.toLowerCase()//Rock compiler aika ba rock rock===rock
-     ComputerSelection=ComputerSelection.toLowerCase()
-        if(playerSelection=='rock' && ComputerSelection=='paper'){
-               computer++;
-            return `You lose! ${ComputerSelection} beats ${playerSelection}`
-            
-        }
-        else if(playerSelection=='paper' && ComputerSelection=='rock'){
-            player++;
-            return  `You win! ${playerSelection} beats ${ComputerSelection}`
-        }
-        else if(playerSelection=='scissors' && ComputerSelection=='rock'){
-               computer++;
-               return `You lose! ${ComputerSelection} beats ${playerSelection}`  
-        }
-          else if(playerSelection=='rock' && ComputerSelection=='scissors'){
-             player++;
-            return  `You win! ${playerSelection} beats ${ComputerSelection}`
-        }
-        else if(playerSelection=='scissors' && ComputerSelection=='paper'){
-             player++;
-             return  `You win! ${playerSelection} beats ${ComputerSelection}`
-        }
-       else if( playerSelection=='paper' && ComputerSelection=='scissors'){
-           computer++;
-         return  `You lose! ${playerSelection} beats ${ComputerSelection}`
-       }
-        else if(playerSelection===ComputerSelection){
-            return 'Tied'
-        }
-        else{
-          return "seems like you didn't write what I told you"    
-        } 
-}
-// function playRound(playerSelection,computerSelection){
-//     const winConditions={
-//         rock :{scissors: true },
-//         paper :{rock: true},
-//         scissors:{paper:true}
-//     }
-//     playerSelection=playerSelection.toLowerCase();
-//     computerSelection=computerSelection.toLowerCase();
-//     if(playerSelection==computerSelection){
-//         return 'Tied'
-//     }
-//     else if(winConditions[playerSelection][computerSelection]){
-//         player++;
-//         return  `You win! ${playerSelection} beats ${computerSelection}`;
-//     }
-//     else{
-//         computer++;
-//         return `You lose! ${playerSelection} beats ${computerSelection}`;
-//     }
+
+    rock.addEventListener('click', ()=>{
+        const computerSelection=getComputerChoice();
+        const playerSelection='rock';
+        playRound(playerSelection,computerSelection);
+    });
+    paper.addEventListener('click', ()=>{
+        const computerSelection=getComputerChoice();
+        const playerSelection='paper';
+        playRound(playerSelection,computerSelection);
+    });
+    scissors.addEventListener('click', ()=>{
+        const computerSelection=getComputerChoice();
+        const playerSelection='scissors';
+        playRound(playerSelection,computerSelection);
+    });
+
+
     
-// }
-function playGame(playerSelection,computerSelection){
-        return playRound(playerSelection,computerSelection) 
-}
+function playRound(playerSelection,computerSelection){
+    console.log(computerSelection)
+    playerSelection.toLowerCase();
+    const winConditions={
+        rock:{scissors:true},
+        paper:{rock:true},
+        scissors:{paper:true}
+    }
+    if(playerSelection==computerSelection){
+               result.textContent='Tied'
+            return outCome.append(result);
+        }
+        else if(winConditions[playerSelection][computerSelection]){
+            playerScore++;
+            result.textContent=`You win! ${playerSelection} beats ${computerSelection}`;
+            return  outCome.append(result);
+        }
+        else {
+               computerScore++;
+               result.textContent=`You lose! ${computerSelection} beats ${playerSelection}`;
+               return outCome.append(result);
+        }
+    }
 
 
-
-
-const playerSelection = prompt('Choose Your weapon(Rock,Paper,Scissors',`Rock,paper,scissors`);
-const computerSelection = getComputerChoice()
-
-console.log(playRound(playerSelection, computerSelection));
-
-
-if(playerWon>computerWon){
+if(player>computer){
     console.log('The User has won the game')
 }
-else if(computerWon>playerWon){
+else if(computer>player){
     console.log('The computer has won the game')
 }
 else{
     console.log('Its A Drawww')
 }
+
