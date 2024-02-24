fb91de0b3737f313d9e2664e52ef06cb5e71b39b
Ara404E <119130650+Ara404E@users.noreply.github.com>
2024-02-24 15:41:59 +0300

    finished Product

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,45 +1,82 @@
 
-    const rock=document.querySelector('#rock');
-    const paper=document.querySelector('#paper');
-    const scissors=document.querySelector('#scissors');
+    const rock=document.querySelector('#rockImg');
+    const paper=document.querySelector('#paperImg');
+    const scissors=document.querySelector('#scissorsImg');
     const weaponDiv=document.querySelector('.weaponDiv');
-    const outCome=document.querySelector('.outcome');
-    const result=document.querySelector('.result');
-    const computer=document.querySelector('.computerScore');
-    const player=document.querySelector(".PlayerScore");
-    
+    const outComeDiv=document.querySelector('.outCome');
+    const h3=document.createElement('h3');
+    h3.className="result";
+    const p=document.createElement('p');
+    const p2=document.querySelector('.compScore');
+    const p3=document.querySelector('.playerScore');
 let playerScore=0;
 let computerScore=0;
-
-
+let winner=5;
 
 function getComputerChoice(){
         let choices=['Rock','Paper','Scissors']
         let picked=choices[Math.floor(Math.random() * choices.length)];
         return picked.toLowerCase()
 }
+function checkWinner(playerScore,compScore){
+    
+    if(playerScore===winner){
+        p.textContent='Great Job you won agains a computer hoorayyy';
+        return outComeDiv.append(p);
+        }
+     if(compScore===winner){
+          p.textContent='Bruhh aint no way you lost to a computer💀';
+        return outComeDiv.append(p);
+    }
+}
 
-
+function checkScore(playerScore,compScore){
+        p3.textContent="user:"+ playerScore;
+        p2.textContent="computer:"+compScore;
+     outComeDiv.append(p3);
+      outComeDiv.append(p2);
+}
+let clickable=true;
     rock.addEventListener('click', ()=>{
-        const computerSelection=getComputerChoice();
+        if(!clickable)return;
+       const computerSelection=getComputerChoice();
         const playerSelection='rock';
-        playRound(playerSelection,computerSelection);
-    });
+       playRound(playerSelection,computerSelection);
+        checkScore(playerScore,computerScore);
+        checkWinner(playerScore,computerScore);
+       if (playerScore >= winner || computerScore >= winner) {
+        clickable=false;
+    }
+       });
+
     paper.addEventListener('click', ()=>{
+        if(!clickable)return;
+
         const computerSelection=getComputerChoice();
         const playerSelection='paper';
         playRound(playerSelection,computerSelection);
+        checkScore(playerScore,computerScore);
+         checkWinner(playerScore,computerScore);
+        if (playerScore >= winner || computerScore >= winner) {
+            clickable=false;
+    }
+
     });
     scissors.addEventListener('click', ()=>{
+        if(!clickable) return;
         const computerSelection=getComputerChoice();
         const playerSelection='scissors';
         playRound(playerSelection,computerSelection);
+        checkScore(playerScore,computerScore);
+        checkWinner(playerScore,computerScore);
+        if (playerScore >= winner || computerScore >= winner) {
+        clickable=false;
+    }
     });
 
 
     
 function playRound(playerSelection,computerSelection){
-    console.log(computerSelection)
     playerSelection.toLowerCase();
     const winConditions={
         rock:{scissors:true},
@@ -47,29 +84,21 @@ function playRound(playerSelection,computerSelection){
         scissors:{paper:true}
     }
     if(playerSelection==computerSelection){
-               result.textContent='Tied'
-            return outCome.append(result);
+               h3.textContent='Tied'
+            return outComeDiv.append(h3);
         }
         else if(winConditions[playerSelection][computerSelection]){
             playerScore++;
-            result.textContent=`You win! ${playerSelection} beats ${computerSelection}`;
-            return  outCome.append(result);
+            h3.textContent=`You win! ${playerSelection} beats ${computerSelection}`;
+            return  outComeDiv.appendChild(h3);
         }
         else {
-               computerScore++;
-               result.textContent=`You lose! ${computerSelection} beats ${playerSelection}`;
-               return outCome.append(result);
+            computerScore++; 
+               h3.textContent=`You lose! ${computerSelection} beats ${playerSelection}`;
+               return  outComeDiv.append(h3);
         }
     }
-
-
-if(player>computer){
-    console.log('The User has won the game')
+        
+function restartBrowser() {
+ location.reload();
 }
-else if(computer>player){
-    console.log('The computer has won the game')
-}
-else{
-    console.log('Its A Drawww')
-}
-
